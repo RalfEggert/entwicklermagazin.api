38f9ff8d56a227c4253f71e93fae707f16d68028
Ralf Eggert <ralf@travello.com>
2017-07-26 06:29:49 +0200

    Deleagator Factories

---
+++ module/App/src/Pipeline/PipelineDelegatorFactory.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * entwicklermagazin.api
+ *
+ * @package    MODULENAME
+ * @copyright  Copyright (c) 2014 ralf
+ * @license    All rights reserved
+ */
+
+
+namespace App\Pipeline;
+
+
+class PipelineDelegatorFactory
+{
+
+}
